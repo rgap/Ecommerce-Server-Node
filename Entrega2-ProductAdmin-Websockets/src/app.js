36e7c286969f77234b639183dfef892c15f84309
rgap <r.guzmanap@gmail.com>
2024-08-05 17:10:55 -0500

    new routes

--- Entrega2-ProductAdmin-Websockets/src/app.js
+++ Entrega2-ProductAdmin-Websockets/src/app.js
@@ -11,12 +11,12 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Handlebars setup
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "src/views"));
 
 // Routes
 app.use("/api/products", productRouter);

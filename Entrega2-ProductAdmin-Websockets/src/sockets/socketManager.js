ce06c34af00892c05c4e59346897301af4baa0c8
rgap <r.guzmanap@gmail.com>
2024-08-06 17:44:30 -0500

    renombrado

--- Entrega2-ProductAdmin-Websockets/src/sockets/socketManager.js
+++ Entrega2-ProductAdmin-Websockets/src/sockets/socketManager.js
@@ -4,9 +4,9 @@ import ProductManager from "../managers/ProductManager.js";
 const productManager = new ProductManager();
 
 export const initSocket = server => {
-  const io = new Server(server);
+  const socketServer = new Server(server);
 
-  io.on("connection", socket => {
+  socketServer.on("connection", socket => {
     console.log("A user connected");
 
     socket.on("disconnect", () => {
@@ -18,7 +18,7 @@ export const initSocket = server => {
       try {
         const savedProduct = await productManager.addProduct(product);
         if (savedProduct) {
-          io.emit("productAdded", savedProduct);
+          socketServer.emit("productAdded", savedProduct);
         }
       } catch (error) {
         console.error("Error adding product:", error);
@@ -30,7 +30,7 @@ export const initSocket = server => {
       try {
         const success = await productManager.deleteProduct(productId);
         if (success) {
-          io.emit("productRemoved", productId);
+          socketServer.emit("productRemoved", productId);
         }
       } catch (error) {
         console.error("Error removing product:", error);
